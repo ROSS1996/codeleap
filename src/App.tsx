4f10c433605de3d2e48190d248df610a002bc7be
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-04-25 14:11:39 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Welcome to CodeLeap network!</h2>
+        <form>
+          <label htmlFor="username">Please enter your username</label>
+          <input type="username" />
+          <button>Enter</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
