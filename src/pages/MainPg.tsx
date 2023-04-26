a0abfccbd8bbc81f5e633a10cfeaa081e3704f42
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-04-25 21:12:38 -0300

    Add post request

--- src/pages/MainPg.tsx
+++ src/pages/MainPg.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import Post from "../components/Post";
 import "./css/MP.css";
 import { State } from "../redux/store";
+import { useNavigate } from "react-router-dom";
 
 interface PostData {
   id: number;
@@ -44,15 +45,11 @@ function PostList({ posts }: { posts: PostData[] }) {
 
 function MainPage() {
   const usernameLocal = useSelector((state: State) => state.username);
-
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-  };
-
   const [posts, setPosts] = useState<PostData[]>([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("https://dev.codeleap.co.uk/careers/?format=json")
@@ -60,6 +57,43 @@ function MainPage() {
       .then((data) => setPosts(data.results))
       .catch((error) => console.error(error));
   }, []);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!usernameLocal || !title || !content) {
+      return;
+    }
+
+    const response = await fetch("https://dev.codeleap.co.uk/careers/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: usernameLocal,
+        title,
+        content,
+      }),
+    });
+
+    if (response.ok) {
+      alert("Post created successfully!");
+      // Re-fetch the list of posts
+      fetch("https://dev.codeleap.co.uk/careers/?format=json")
+        .then((response) => response.json() as Promise<APIResponse>)
+        .then((data) => setPosts(data.results))
+        .catch((error) => console.error(error));
+      setTitle("");
+      setContent("");
+    } else {
+      console.error(response.statusText);
+    }
+  };
+
+  if (!usernameLocal) {
+    navigate("/");
+  }
 
   return (
     <>
@@ -80,14 +114,7 @@ function MainPage() {
               value={content}
               onChange={(e) => setContent(e.target.value)}
             />
-            <button
-              type="submit"
-              disabled={
-                title.length === 0 ||
-                content.length === 0 ||
-                usernameLocal === null
-              }
-            >
+            <button type="submit" disabled={!title || !content}>
               Create
             </button>
           </form>
